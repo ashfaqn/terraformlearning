1ea97eec42a218593453f9e81d1281883b02ef9b
Nouman Ashfaq <noumanashfaq@outlook.com>
2018-08-19 22:26:53 +0500

    output, variable sections added

--- main.tf
+++ main.tf
@@ -2,12 +2,40 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "redhat" {
-  ami = "ami-6871a115"
+variable "server_port" {
+  description = "value of web server port"
+  default = 8080
+}
+
+resource "aws_instance" "ubuntu" {
+  ami = "ami-759bc50a"
   instance_type = "t2.micro"
+
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "hello world" > index.html
+              nohup busybox httpd -f -p "${var.server_port}" &
+              EOF
 
   tags {
     Name = "terraform-example"
   }
+}
 
+resource "aws_security_group" "instance" {
+  name = "terraform security group example"
+
+  ingress{
+    from_port = "${var.server_port}"
+    to_port = "${var.server_port}"
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+output "public_ip" {
+  value = "${aws_instance.ubuntu.public_ip}"
 }
